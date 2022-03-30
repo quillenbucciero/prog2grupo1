1620602648f68f866b9bfa598bd2367b0001b8d6
carocanorio <carocanorio@gmail.com>
2022-03-30 19:45:21 -0300

    hice las BASES de db

---
+++ myApp/db/data.js
@@ -0,0 +1,25 @@
+const data = {
+    usuario: {
+        usuario: 'texto',
+        email: 'campo de tipo email',
+        contrasenia: 'texto',
+        fechaDeNacimiento: 21/06/03,
+        documento: 4480000,
+        fotoDePerfil: 'campo de tipo file'
+    },
+    producto: [
+        {
+            imagen: 'campo de tipo file',
+            nombreProducto: 'algo',
+            descripcion: 'texto',
+            fechaDeCarga: 22/03/22
+        },
+    ],
+    comentarios: [
+        {
+            usuario: 'nombre',
+            textoDelComentario: 'texto',
+            imagenDePerfil: 'file'
+        },
+    ]
+}
