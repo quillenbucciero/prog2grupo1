e16d14d02a040226fa2451f1c18ec9413f392a41
LolaBiscay <lbiscay@udesa.edu.ar>
2022-04-06 19:13:53 -0300

    arreglando

--- controllers/profileController.js
+++ controllers/profileController.js
@@ -3,18 +3,14 @@ let data = require('../db/data');
 const profileController = {
 
     index: function(req, res) {
-
-
         return res.render('profile',{
-            data: data.usuario
+            data: data.usuario,
+            productos: data.producto
         })
-
     },
-
     edit: function(req, res) {
        return res.render('profile-edit');
-
-    }
+    },
 };
 
 module.exports = profileController;
