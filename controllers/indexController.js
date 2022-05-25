265de8cb6107975bbe1255ae56d91f4f967bf37f
carocanorio <ccanoriohellwing@udesa.edu.ar>
2022-05-25 18:23:35 -0300

    puse las rutas creadas anteriormente en index

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -1,4 +1,9 @@
 const data = require('../db/data');
+const db = require("../database/models"); //Requiero db CAMBIAR
+const user = db.User;
+
+/* Requerir mi modulo instalado */
+const bcrypt = require('bcryptjs');
 
 const indexController = {
     index: function (req,res) {
@@ -9,9 +14,58 @@ const indexController = {
     register: function (req,res) {
         return res.render('register')
     },
+    procesarRegister : (req, res) => {
+        let info = req.body;
+        let usuario = {
+            name : info.name,
+            email : info.email,
+            password : bcrypt.hashSync(info.password, 10),
+            remember_token : "false",
+            created_at : new Date(),
+            updated_at :  new Date(),
+        }
+
+        user.create(usuario)
+        .then((result) => {
+            return res.redirect("/profile")
+        }).catch((err) => {
+            return res.send(err)
+        });
+        
+    },
     login: function (req,res) {
         return res.render('login')
-    }
+    },
+    procesarLogin : (req, res) => {
+        let info = req.body;
+        let filtro = {where : [ { email : info.email}]};
+
+        user.findOne(filtro)
+        .then((result) => {
+            
+            if (result != null) {
+
+                let passEncriptada = bcrypt.compareSync(info.password , result.password)
+                if (passEncriptada) {
+                    return res.redirect("/profile")
+                } else {
+                    return res.send("Existe el mail " +  result.email + " pero la clave es incorrecta");
+                }
+               
+            } else {
+                return res.send("No existe este mail " +  info.email);
+            }
+
+        }).catch((err) => {
+            
+        });
+
+    },
 };
 
 module.exports = indexController;
+
+
+
+
+
