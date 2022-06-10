2d9ae296ab8307b4b543c9da3a7202f821548282
LolaBiscay <lbiscay@udesa.edu.ar>
2022-06-10 17:57:35 -0300

    Trabajando con el buscador

--- controllers/search-resultsController.js
+++ controllers/search-resultsController.js
@@ -1,27 +1,39 @@
 const db = require("../database/models"); //Requiero db 
-const productos = db.Productos; //Alias de la db
+const Productos = db.Productos; //Alias de la db
+const op = db.Sequelize.Op;
 
 const searchController = {
     index: function(req,res){  
-        
-        let palabraBuscada = req.query.search;  
-        let productosEncontrados = []; 
-        
-        for (let i = 0; i < data.producto.length; i++) {
-            if (data.producto[i].nombreProducto.toLowerCase().includes(palabraBuscada)) {
-                productosEncontrados.push(data.producto[i])
-            }
-            
-        }
-
-        return res.render('search-results',{
-            productos: productosEncontrados,
-            buscado : palabraBuscada
-        });
-
-
-       
+        let palabraBuscada = req.query.search; /* search es el input name del formulario de los headers*/ 
+        let promesaNombre = Productos.findAll({
+                                where:[
+                                    {nombre:{ [op.like]: `%${palabraBuscada}%`}}
+                                ] 
+                            });
+        let promesaDescripcion = Productos.findAll({
+                                    where:[
+                                        {descripcion:{ [op.like]: `%${palabraBuscada}%`}}
+                                    ] 
+                                });
+        Promise.all([promesaNombre, promesaDescripcion])
+        .then(function([resNombre, resDescripcion]){
+            let arrDeResultados = [];
+            for (let i = 0; i < resNombre.length; i++) {
+                arrDeResultados.push(resNombre[i])
+            } 
+            for (let i = 0; i < resDescripcion.length; i++) {
+                arrDeResultados.push(resDescripcion[i])
+            } 
+            res.render('search-results', {
+                resultados: arrDeResultados
+            })
+        })
+        .catch(err => console.log(err));
     }
+
+  /*EN CASO DE NO ENCONTRAR RTDOS MOSTRAR MENSAJE*/
+
+
 };
 
 module.exports = searchController;
--- routes/search-results.js
+++ routes/search-results.js
@@ -3,6 +3,6 @@ const router = express.Router();
 const searchController = require('../controllers/search-resultsController')
 
 /* GET home page. */
-router.get('/id/:id?', searchController.index);
+router.get('/', searchController.index);
 
 module.exports = router;
